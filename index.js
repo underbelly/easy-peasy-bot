92b4264e9a83c35ee289f48748f2459535946227
Craig Tingey <craig@decisely.com>
2019-11-19 16:21:40 -0700

    interactive messaging working

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const Messages = require('./messages');
-// import * as Messages from './messages';
+var express = require("express");
+var bodyParser = require("body-parser");
+
+const StartGameMessage = require("./messages");
 /**
  * A Bot for Slack!
  */
-
 
 /**
  * Define a function for initiating a conversation on installation
@@ -13,18 +14,19 @@ const Messages = require('./messages');
  */
 
 function onInstallation(bot, installer) {
-    if (installer) {
-        bot.startPrivateConversation({user: installer}, function (err, convo) {
-            if (err) {
-                console.log(err);
-            } else {
-                convo.say('I am a bot that has just joined your team');
-                convo.say('You must now /invite me to a channel so that I can be of use!');
-            }
-        });
-    }
+  if (installer) {
+    bot.startPrivateConversation({ user: installer }, function(err, convo) {
+      if (err) {
+        console.log(err);
+      } else {
+        convo.say("I am a bot that has just joined your team");
+        convo.say(
+          "You must now /invite me to a channel so that I can be of use!"
+        );
+      }
+    });
+  }
 }
-
 
 /**
  * Configure the persistence options
@@ -32,22 +34,16 @@ function onInstallation(bot, installer) {
 
 var config = {};
 if (process.env.MONGOLAB_URI) {
-    var BotkitStorage = require('botkit-storage-mongo');
-    config = {
-        storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
-    };
-} else if (process.env.REDIS_DB) {
-    redisConfig = {
-      methods: ['gary','other']
-    }
-    redisStorage = require('botkit-storage-redis')(redisConfig),
-    config = {
-        storage: redisStorage
-    };
+  var BotkitStorage = require("botkit-storage-mongo");
+  config = {
+    storage: BotkitStorage({ mongoUri: process.env.MONGOLAB_URI })
+  };
 } else {
-    config = {
-        json_file_store: ((process.env.TOKEN)?'./db_slack_bot_ci/':'./db_slack_bot_a/'), //use a different name if an app or CI
-    };
+  config = {
+    json_file_store: process.env.TOKEN
+      ? "./db_slack_bot_ci/"
+      : "./db_slack_bot_a/" //use a different name if an app or CI
+  };
 }
 
 /**
@@ -55,169 +51,114 @@ if (process.env.MONGOLAB_URI) {
  */
 
 if (process.env.TOKEN || process.env.SLACK_TOKEN) {
-    //Treat this as a custom integration
-    var customIntegration = require('./lib/custom_integrations');
-    var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN;
-    var controller = customIntegration.configure(token, config, onInstallation);
-} else if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
-    //Treat this as an app
-    var app = require('./lib/apps');
-    var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
+  //Treat this as a custom integration
+  var customIntegration = require("./lib/custom_integrations");
+  var token = process.env.TOKEN ? process.env.TOKEN : process.env.SLACK_TOKEN;
+  var controller = customIntegration.configure(token, config, onInstallation);
+} else if (
+  process.env.CLIENT_ID &&
+  process.env.CLIENT_SECRET &&
+  process.env.PORT
+) {
+  //Treat this as an app
+  var app = require("./lib/apps");
+  var controller = app.configure(
+    process.env.PORT,
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    config,
+    onInstallation
+  );
 } else {
-    console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment');
-    process.exit(1);
+  console.log(
+    "Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment"
+  );
+  process.exit(1);
 }
 
-
-/**
- * A demonstration for how to handle websocket events. In this case, just log when we have and have not
- * been disconnected from the websocket. In the future, it would be super awesome to be able to specify
- * a reconnect policy, and do reconnections automatically. In the meantime, we aren't going to attempt reconnects,
- * WHICH IS A B0RKED WAY TO HANDLE BEING DISCONNECTED. So we need to fix this.
- *
- * TODO: fixed b0rked reconnect behavior
- */
-// Handle events related to the websocket connection to Slack
-controller.on('rtm_open', function (bot) {
-    console.log('** The RTM api just connected!');
+controller.on("rtm_open", function(bot) {
+  console.log("** The RTM api just connected!");
 });
 
-controller.on('rtm_close', function (bot) {
-    console.log('** The RTM api just closed');
-    // you may want to attempt to re-open
+controller.on("rtm_close", function(bot) {
+  console.log("** The RTM api just closed");
+  // you may want to attempt to re-open
 });
-
 
 /**
  * Core bot logic goes here!
  */
 // BEGIN EDITING HERE!
 
-
-controller.on('bot_channel_join', function (bot, message) {
-    bot.reply(message, "I'm here!")
+controller.on("bot_channel_join", function(bot, message) {
+  bot.reply(message, "I'm back from the dead!");
 });
 
-controller.hears('hello', ['direct_mention', 'mention', 'direct_message'], function (bot, message) {
-    bot.reply(message, 'Hello!');
-});
-
-controller.hears('hi', ['direct_mention', 'mention', 'direct_message'], function (bot, message) {
-    bot.reply(message, 'Yolo!2');
-});
-
-controller.hears('add', ['direct_mention', 'mention', 'direct_message'], function (bot, message) {
-    bear = {id: 'cool', beans: ['cool', 'gray']}
-    controller.storage.teams.save(bear, error => console.log(error));
-    bot.reply(message, 'Saved a bear');
-});
-
-controller.hears('test', ['direct_mention', 'mention', 'direct_message'], function (bot, message) {
-    console.log('here');
-    bugger = controller.storage.teams.get('cool', error => console.log(error))
+controller.hears(
+  "test",
+  ["direct_mention", "mention", "direct_message"],
+  function(bot, message) {
+    console.log("here");
+    bugger = controller.storage.teams.get("cool", error => console.log(error));
     console.log(bugger);
-    console.log(controller.storage.teams)
-    console.log(controller.storage.teams.all(cb => null), {})
-    bot.reply(message, 'testing 1..2..3..');
+    console.log(controller.storage.teams);
+    console.log(
+      controller.storage.teams.all(cb => null),
+      {}
+    );
+    bot.reply(message, "testing 1..2..3..");
+  }
+);
+
+
+// Variables
+let tempStore = [];
+let GameInitiated = false;
+
+controller.hears("reset", ["direct_mention", "mention", "direct_message"], function(bot, message) {
+  tempStore = [];
+  GameInitiated = false;
+  bot.reply(message, "$#%@ system error... Everything has been reset.");
 });
 
-controller.hears('play', ['direct_mention', 'mention', 'direct_message'], function (bot, message) {
-    console.log(Messages);
-    bot.reply(message, {attachments:[Messages.startGameMessage]});
-    bot.replyInteractive(message, 'huh?');
+controller.hears(
+  "play",
+  ["direct_mention", "mention", "direct_message"],
+  function(bot, message) {
+    // const payload = JSON.parse(message.payload);
+    // const username = payload.user.name;
+
+    if (GameInitiated) {
+      bot.reply(message, "A game has already been started, click \"Join\" if you want to play");
+    } else {
+      GameInitiated = true;
+      bot.reply(message, {
+        attachments: [StartGameMessage([])]
+      });
+    }
+  }
+);
+
+
+controller.on("interactive_message_callback", function(bot, message) {
+  // console.log("in here", message.callback_id, message.actions);
+  const payload = JSON.parse(message.payload);
+  const username = payload.user.name;
+  const action = message.actions[0].value;
+
+  if (action === 'add_user_to_game') {
+    tempStore.includes(username) || tempStore.length >= 4 ? null : tempStore.push(username);
+  } else if (action === 'remove_user_from_game') {
+    const index = tempStore.indexOf(username);
+    if (index !== -1) {
+      tempStore = tempStore.filter(i => i !== username);
+    }
+  }
+
+  bot.replyInteractive(message, {
+      attachments: [
+      StartGameMessage(tempStore)
+    ]
+  });
 });
 
-controller.on('block_actions', function(bot, message) {
-    console.log('action happened');
-    bot.reply(message, 'what?');
-})
-
-
-//  TESTING STUFF
-controller.hears('interactive', 'direct_message', function(bot, message) {
-
-    bot.reply(message, {
-        attachments:[
-            {
-                title: 'Do you want to interact with my buttons?',
-                callback_id: 'block_actions',
-                attachment_type: 'default',
-                actions: [
-                    {
-                        "name":"yes",
-                        "text": "Yes",
-                        "value": "yes",
-                        "type": "button",
-                    },
-                    {
-                        "name":"no",
-                        "text": "No",
-                        "value": "no",
-                        "type": "button",
-                    }
-                ]
-            }
-        ]
-    });
-});
-
-controller.on('block_actions', function(bot, message) {
-  console.log('in block actions')
-});
-
-controller.on('interactive_message_callback', function(bot, message) {
-
-    // check message.actions and message.callback_id to see what action to take...
-    console.log('in here');
-
-    bot.replyInteractive(message, {
-        text: '...',
-        attachments: [
-            {
-                title: 'My buttons',
-                callback_id: '123',
-                attachment_type: 'default',
-                actions: [
-                    {
-                        "name":"yes",
-                        "text": "Yes!",
-                        "value": "yes",
-                        "type": "button",
-                    },
-                    {
-                       "text": "No!",
-                        "name": "no",
-                        "value": "delete",
-                        "style": "danger",
-                        "type": "button",
-                        "confirm": {
-                          "title": "Are you sure?",
-                          "text": "This will do something!",
-                          "ok_text": "Yes",
-                          "dismiss_text": "No"
-                        }
-                    }
-                ]
-            }
-        ]
-    });
-
-});
-
-
-/**
- * AN example of what could be:
- * Any un-handled direct mention gets a reaction and a pat response!
- */
-//controller.on('direct_message,mention,direct_mention', function (bot, message) {
-//    bot.api.reactions.add({
-//        timestamp: message.ts,
-//        channel: message.channel,
-//        name: 'robot_face',
-//    }, function (err) {
-//        if (err) {
-//            console.log(err)
-//        }
-//        bot.reply(message, 'I heard you loud and clear boss.');
-//    });
-//});
